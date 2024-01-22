1174bb7557dcfe8caca6a0cb45ded5701a2ed79d
ananthanil <ab.ananth97@gmail.com>
2024-01-22 18:39:38 +0530

    added nullish

--- src/component/Rbutton.jsx
+++ src/component/Rbutton.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import '../component/Stylebutton.css'
+import React from "react";
+import "../component/Stylebutton.css";
 
 function Rbutton(props) {
-    const{label}=props
-    return (
-        <div>
-            <button
-            className='login_btn'
-            >{label}</button>
-        </div>
-    );
+  const { label } = props;
+  return (
+    <div>
+      <button className="login_btn">{label ?? "Default"}</button>
+    </div>
+  );
 }
 
 export default Rbutton;
--- src/page/login.jsx
+++ src/page/login.jsx
@@ -1,32 +1,28 @@
-import React from 'react';
-import Rbutton from '../component/Rbutton';
-import Rtext from '../component/Rtext';
-import '../page/style.css'
+import React from "react";
+import Rbutton from "../component/Rbutton";
+import Rtext from "../component/Rtext";
+import "../page/style.css";
 
 function login(props) {
-    return (
-        <div className='App'>
+  return (
+    <div className="App">
+      <h1 style={{ color: "white" }}>Log In</h1>
 
-            <h1 style={{color:'white'}}>Log In</h1>
+      <Rtext
+        style={{ width: "375px", height: "30px", marginBottom: "15px" }}
+        type={"text"}
+        placeholder={"Username"}
+      ></Rtext>
 
-            <Rtext
-            style={{width:"375px",height:"30px",marginBottom:"15px"}}
-            type={"text"}
-            placeholder={"Username"}
-            ></Rtext>
+      <Rtext
+        style={{ width: "375px", height: "30px", marginBottom: "15px" }}
+        type={"password"}
+        placeholder={"Password"}
+      ></Rtext>
 
-            <Rtext
-            style={{width:"375px",height:"30px",marginBottom:"15px"}}
-             type={"password"}
-             placeholder={"Password"}
-            ></Rtext>
-
-            <Rbutton
-            label={'LOG IN'}
-            ></Rbutton> 
-
-        </div>
-    );
+      <Rbutton label={"LOG IN"}></Rbutton>
+    </div>
+  );
 }
 
 export default login;
